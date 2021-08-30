2e579c4c852e79636d9482aaf337bc72f9e4bf85
dada199 <forrestshi199@hotmail.com>
2021-08-30 22:27:37 +0800

    update

--- test/main_test.py
+++ test/main_test.py
@@ -157,19 +157,7 @@ if __name__ == '__main__':
 
     print('S09 ' * 10 + ' Sale stock_code="SZ.600301", stock_name="ZSZQ", sale_number=2000, sale_price=14.00 ' + 'S ' * 5)
 
-    account.sale(stock_code="SZ.600301", stock_name="ZSZQ", sale_number=5000, sale_price=14.00, sale_date="2021-08-26",
-                 trade_flag='S')
-    total_asset = account.get_total_asset()
-    stock_value = account.get_stock_value()
-    available_asset = account.get_available_asset()
-    profit_and_loss = account.get_profit_and_loss()
-    holding_stock_info = account.get_holding_stock()
 
-    print(f'Total asset {total_asset}')
-    print(f'Stock value {stock_value}')
-    print(f'Stock available_asset {available_asset}')
-    print(f'Stock profit_and_loss {profit_and_loss}')
-    print(holding_stock_info.to_string())
 
 
 
